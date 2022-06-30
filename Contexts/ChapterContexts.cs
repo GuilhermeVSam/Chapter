b8de2d6b8a7030b35d495d6332fd5686b280b385
Guilherme <guiwojtysiak053@hotmail.com>
2022-06-30 16:44:34 -0300

    Push
    Push

---
+++ Contexts/ChapterContexts.cs
@@ -0,0 +1,23 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            optionsBuilder.UseSqlServer("DataSource=DESKTOP-JPQNRAT; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+            public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,34 @@
+﻿using Chapter.Models;
+using Chapter.Repositories;
+using Chapter.Contexts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+        public LivrosController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
